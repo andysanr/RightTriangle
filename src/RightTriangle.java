4b7119d69eca766748eac73789bddf4e145ef3b1
andysantanar <andysantana00@gmail.com>
2023-03-08 20:52:49 -0500

    Initial commit

---
+++ src/RightTriangle.java
@@ -0,0 +1,12 @@
+public class RightTriangle {
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+
+        boolean isRightTriangle = (a * a + b * b == c * c) || (a * a + c * c == b * b) || (b * b + c * c == a * a);
+
+        System.out.println(isRightTriangle);
+
+    }
+}
